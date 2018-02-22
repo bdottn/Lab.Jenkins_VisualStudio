e6dcf7f6f277906e54905365f4907c288a007be3
Echo Lee <bdottn.lee+git@gmail.com>
2018-02-22 17:06:24 +0800

    建立 CalculatorService
    1.　加入方法：Sum
    2.　加入方法：Diffrence
    3.　加入方法：Product
    4.　加入方法：Quotient
    5.　加入方法：Remainder

---
+++ Service/CalculatorService.cs
@@ -0,0 +1,40 @@
+﻿namespace Service
+{
+    public sealed class CalculatorService
+    {
+        public int Sum(int augend, int addend)
+        {
+            return augend + addend;
+        }
+
+        public int Diffrence(int minuend, int subtrahend)
+        {
+            return minuend - subtrahend;
+        }
+
+        public int Product(int mulitplicand, int multiplier)
+        {
+            return mulitplicand * multiplier;
+        }
+
+        public int Quotient(int dividend, int divisor)
+        {
+            if (divisor == 0)
+            {
+                return 0;
+            }
+
+            return dividend / divisor;
+        }
+
+        public int Remainder(int dividend, int divisor)
+        {
+            if (divisor == 0)
+            {
+                return dividend;
+            }
+
+            return dividend % divisor;
+        }
+    }
+}
