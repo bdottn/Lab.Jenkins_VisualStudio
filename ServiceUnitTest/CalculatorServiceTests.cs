9905fbc2cc468f66db54e40556cda7850109ec42
Echo Lee <bdottn.lee+git@gmail.com>
2018-02-27 18:40:27 +0800

    建立 CalculatorServiceTests 單元測試
    1.　加入測試案例：SumTest_傳入augend為10_addend為20_預期回傳30
    2.　加入測試案例：DiffrenceTest_傳入minuend為20_subtrahend為10_預期回傳10

---
+++ ServiceUnitTest/CalculatorServiceTests.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Service.UnitTest
+{
+    [TestClass]
+    public sealed class CalculatorServiceTests
+    {
+        private CalculatorService service;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            this.service = new CalculatorService();
+        }
+
+        [TestMethod]
+        public void SumTest_傳入augend為10_addend為20_預期回傳30()
+        {
+            var augend = 10;
+            var addend = 20;
+
+            var expected = 30;
+
+            var actual = this.service.Sum(augend, addend);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void DiffrenceTest_傳入minuend為20_subtrahend為10_預期回傳10()
+        {
+            var minuend = 20;
+            var subtrahend = 10;
+
+            var expected = 10;
+
+            var actual = this.service.Diffrence(minuend, subtrahend);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
